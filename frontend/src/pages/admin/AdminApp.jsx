942c3c09c35f07a19e0347bd9645b20a5f182271
only1ahmed-developer <only1.ahmed.abdul.hamidu@gmail.com>
2026-08-06 13:40:48 +0300

    Update AdminApp.jsx

--- frontend/src/pages/admin/AdminApp.jsx
+++ frontend/src/pages/admin/AdminApp.jsx
@@ -83,8 +83,8 @@ export default function AdminApp() {
         </div>
       )}
 
-        </nav>
-      </aside>
+      
+   
 
       <main className="flex-1 p-6 pt-20 md:pt-6 overflow-y-auto overflow-x-hidden min-w-0">
         <Routes>
