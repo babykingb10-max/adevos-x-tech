5d3d6022ff7eefaa649bb4ae8acc607299b0e59c
only1ahmed-developer <only1.ahmed.abdul.hamidu@gmail.com>
2026-08-06 13:14:08 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,13 @@ import HeroSlider from "../components/home/HeroSlider";
 import {
   ServicesSection, InTouchSection, SupportSection, TestimonialsSection, StayConnectedSection,
 } from "../components/home/HomeSections";
-import { usePopup } from "../context/PopupContext";
 
 export default function Home() {
-  const { open } = usePopup();
-
   return (
     <div>
       <HeroSlider />
       <ServicesSection />
-      <InTouchSection onOpenPopup={open} />
+      <InTouchSection />
       <SupportSection />
       <TestimonialsSection />
       <StayConnectedSection />
